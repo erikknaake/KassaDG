51407e4b4477a796d6a216089074f37e9aafeb55
Erik Knaake <e.knaake@student.han.nl>
2020-08-17 21:15:30 +0200

    publish script

--- KassaDG/Startup.cs
+++ KassaDG/Startup.cs
@@ -3,6 +3,7 @@ namespace KassaDG
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.SpaServices.AngularCli;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
@@ -39,6 +40,12 @@ namespace KassaDG
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<KassaDgDbContext>();
+                context.Database.Migrate();
+            }
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
---
+++ KassaDG/publish.sh
@@ -0,0 +1,5 @@
+rm -rf ./bin
+rm -rf ./ClientApp/dist
+dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -r linux-x64
+mkdir ./bin/Release/netcoreapp3.1/linux-x64/Persistence
+cp ../Persistence/KassaDG.db ./bin/Release/netcoreapp3.1/linux-x64/Persistence/KassaDG.db
