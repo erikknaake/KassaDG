decd322e666395f0f79154096e1a75322b36b366
Mahtaran <mahtaran@amuzil.com>
2021-06-27 15:55:34 +0200

    Rename launch script in setup

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 cd ..
 xcopy .\\KassaDG_windows-x64 %userprofile%\\.KassaDG_windows-x64\\ /E/H
-copy .\\KassaDG_windows-x64\\Kassa_desktop_launcher.sh %userprofile%\\Desktop\\
+copy .\\KassaDG_windows-x64\\launch.bat %userprofile%\\Desktop\\
 rmdir /s/q .\\KassaDG_windows-x64
